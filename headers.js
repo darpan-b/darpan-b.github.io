cf9a2875405ee7ca456cf95c78ba140716a51118
Darpan Bhattacharya <dbhattacharya170803@gmail.com>
2023-09-25 16:35:36 +0530

    Updated some stuff

---
+++ headers.js
@@ -0,0 +1,18 @@
+
+
+document.write('\
+\
+<TABLE border="0" width="700" align="center">\
+   <TR>\
+      <TD width="100%" align="left" valign="top">\
+		<h3 align="center">\
+		   		<b>\
+		   		<a href="/index.html">Home</a> .\
+         <a href="/My_CV_20230925.pdf">CV</a> .\
+         <a href="mailto:dbhattacharya170803@gmail.com">Contact</a> \
+</b></h3>\
+<br>\
+     </td>\
+   </tr>\
+</table>\
+');
